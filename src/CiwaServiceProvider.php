efacc258b89452b25367c1a1fd9c46e74410b891
Boris DEHOUMON <borisdehoumon@gmail.com>
2021-03-02 02:51:27 +0100

    add column transformer

--- src/CiwaServiceProvider.php
+++ src/CiwaServiceProvider.php
@@ -2,10 +2,8 @@
 
 namespace Dcolsay\Ciwa;
 
-use Dcolsay\Ciwa\Headings\HeadingFormatter;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
 use Spatie\LaravelPackageTools\Package;
+use Dcolsay\Ciwa\Headings\HeadingFormatter;
 use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -36,41 +34,7 @@ class CiwaServiceProvider extends PackageServiceProvider
         });
         // HeadingRowFormatter::extend('ciwa', function($value) {
         //     dd($value);
-        //     $value = Str::lower($value);
-        //     $headings =   [
-        //         'customercode' => 'customer_code',
-        //         'companyname' => 'company_name',
-
-        //         'legalform' => 'legal_form',
-        //         'legalformlookup' => 'legal_form',
-
-        //         'businessstatuslookup' => 'business_status',
-        //         'businessstatus' => 'business_status',
-
-        //         'establishmentdate' => 'establishment_date', // @todo Champ obligatoire
-
-        //         'industrysector' => 'industry_sector',
-        //         'industrysectorlookup' => 'industry_sector',
-
-        //         'registrationnumber' => 'registration_number',
-        //         'identificationnumbersregistrationnumber' => 'registration_number',
-        //         'identificationnumber' => 'registration_number',
-
-        //         'registrationnumberissuercountrylookup' => 'registration_number_issuer_country',
-        //         'identificationnumbersregistrationnumberissuercountrylookup' => 'registration_number_issuer_country',
-
-        //         'mainaddressline' => 'address_line',
-        //         'mainaddressaddressline' => 'address_line',
-
-        //         'mobilephone' => 'mobile_phone',
-        //         'contactsmobilephone' => 'mobile_phone',
-
-        //         'fixedline' => 'fixed_line',
-        //         'contactsfixedline' => 'fixed_line',
-
-        //         'email' => 'email',
-        //         'contactsemail' => 'email',
-        //     ];
+        //     $value = Str::lower($value)
 
         //     $value = (Arr::exists($headings, $value))
         //          ? $headings[$value]
